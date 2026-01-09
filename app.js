516d8fe3ec7dbbae819ad4a1b8bffb66fb85ed7c
Youcef <youcefcopy@gmail.com>
2026-01-09 18:54:47 +0100

    testing access to the cards obj

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-console.log('js has been loaded');
+import { cards } from "./cards.js";
 
 const startBtn = document.getElementById('startBtn');
 console.log(startBtn);
 
 startBtn.addEventListener('click', () => {
     console.log('btn has been clicked');
+    console.log(cards);
 })
