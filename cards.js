d73630350015cb883e510a12837420fcb1779a6f
Youcef <youcefcopy@gmail.com>
2026-01-09 18:51:34 +0100

    adding the card objects

--- cards.js
+++ cards.js
@@ -1,9 +1,18 @@
 // creating the cards object here
 const cards = {
+    card1: {
+        id : 'hello-kitty'
+    },
+    card2: {
+        id: 'my-melody'
+    },
+    card3: {
+        id: 'kurumi'
+    },
+    card4: {
+        id: 'pochacco'
+    }
 
-
-
-
-
-    
 }
+
+export {cards}
