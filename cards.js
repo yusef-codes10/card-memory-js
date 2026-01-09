f6e267b5a6df3e778549eedcd739fffd813b8b0e
Youcef <youcefcopy@gmail.com>
2026-01-09 19:40:29 +0100

    we have to use object literal with arrays

--- cards.js
+++ cards.js
@@ -1,31 +1,32 @@
 // creating the cards object here
-const cards = {
-    card1: {
-        id : 'hello-kitty',
-        img: '/img/hello-kity.jpeg'
-    },
-    card2: {
-        id: 'my-melody',
-        img: '/img/my-melody.jpeg'
-    },
-    card3: {
-        id: 'kurumi',
-        img: '/img/kurumi.jpeg'
-    },
-    card4: {
-        id: 'pochacco',
-        img: '/img/pochacco.jpeg'
-    },
-    card5: {
-        id: 'pompompurin',
-        img: '/img/pompompurin.jpeg'
-    },
-    card6: {
-        id: 'chococat',
-        img: '/img/chococat.jpeg'
+const cards = [
+  {
+    id: 'hello-kitty',
+    img: '/img/hello-kity.jpeg'
+  },
+  {
+    id: 'my-melody',
+    img: '/img/my-melody.jpeg'
+  },
+  {
+    id: 'kurumi',
+    img: '/img/kurumi.jpeg'
+  },
+  {
+    id: 'pochacco',
+    img: '/img/pochacco.jpeg'
+  },
+  {
+    id: 'pompompurin',
+    img: '/img/pompompurin.jpeg'
+  },
+  {
+    id: 'chococat',
+    img: '/img/chococat.jpeg'
+  }
+];
 
-    }
+export { cards };
 
-}
 
 export {cards}
