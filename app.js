9b8fc9c91b9ead731bb6087e811e2d25a435bfe7
Youcef <youcefcopy@gmail.com>
2026-01-11 09:36:35 +0100

    finding the exact img data using the find() method

--- app.js
+++ app.js
@@ -40,8 +40,11 @@ function loadCards() {
 function flipCard() {
     // calling the regular load function
 
-    // make sure we clicked an image
+    // 1️⃣ make sure we clicked an image
     if (!e.target.classList.contains('dynamic-card')) return;
+
+     // 2️⃣ find card data, which was stored on the dataset propery
+     const card = cards.find(c => c.id === img.dataset.id);
 
     cards.forEach(card => {
         const image = document.createElement('img');
