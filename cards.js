e0b7bad8e8dfd5558b40955ef78fc0b4a7397fb7
Youcef <youcefcopy@gmail.com>
2026-01-20 16:50:40 +0100

    removing the flipped from the modal (card) and adding it to the UI (DOM)

--- cards.js
+++ cards.js
@@ -3,38 +3,32 @@ const cards = [
   {
     id: 'hello-kitty',
     img: '/img/hello-kitty.jpeg',
-    bg: '/img/card-back.jpeg',
-    flipped: false
+    bg: '/img/card-back.jpeg'
   },
   {
     id: 'my-melody',
     img: '/img/my-melody.jpeg',
-    bg: '/img/card-back.jpeg',
-    flipped: false
+    bg: '/img/card-back.jpeg'
   },
   {
     id: 'kuromi',
     img: '/img/kuromi.jpeg',
-    bg: '/img/card-back.jpeg',
-    flipped: false
+    bg: '/img/card-back.jpeg'
   },
   {
     id: 'pochacco',
     img: '/img/pochacco.jpeg',
-    bg: '/img/card-back.jpeg',
-    flipped: false
+    bg: '/img/card-back.jpeg'
   },
   {
     id: 'pompompurin',
     img: '/img/pompompurin.jpeg',
-    bg: '/img/card-back.jpeg',
-    flipped: false
+    bg: '/img/card-back.jpeg'
   },
   {
     id: 'chococat',
     img: '/img/chococat.jpeg',
-    bg: '/img/card-back.jpeg',
-    flipped: false
+    bg: '/img/card-back.jpeg'
   }
 ];
 
