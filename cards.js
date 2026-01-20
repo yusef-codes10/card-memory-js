fc173eaba99ca6f6835bd2df19ad449182645420
Youcef <youcefcopy@gmail.com>
2026-01-20 16:42:05 +0100

    adding the flipped property | to compare & allow only two to be flipped at a time

--- cards.js
+++ cards.js
@@ -3,32 +3,38 @@ const cards = [
   {
     id: 'hello-kitty',
     img: '/img/hello-kitty.jpeg',
-    bg: '/img/card-back.jpeg'
+    bg: '/img/card-back.jpeg',
+    flipped: false
   },
   {
     id: 'my-melody',
     img: '/img/my-melody.jpeg',
-    bg: '/img/card-back.jpeg'
+    bg: '/img/card-back.jpeg',
+    flipped: false
   },
   {
     id: 'kuromi',
     img: '/img/kuromi.jpeg',
-    bg: '/img/card-back.jpeg'
+    bg: '/img/card-back.jpeg',
+    flipped: false
   },
   {
     id: 'pochacco',
     img: '/img/pochacco.jpeg',
-    bg: '/img/card-back.jpeg'
+    bg: '/img/card-back.jpeg',
+    flipped: false
   },
   {
     id: 'pompompurin',
     img: '/img/pompompurin.jpeg',
-    bg: '/img/card-back.jpeg'
+    bg: '/img/card-back.jpeg',
+    flipped: false
   },
   {
     id: 'chococat',
     img: '/img/chococat.jpeg',
-    bg: '/img/card-back.jpeg'
+    bg: '/img/card-back.jpeg',
+    flipped: false
   }
 ];
 
